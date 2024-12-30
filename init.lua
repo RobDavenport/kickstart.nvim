b8ced2a0ca50d49b4bb04dea76ce9f82cfffcb22
RobDavenport <RobDavenport>
2024-12-30 15:14:57 +0900

    oil show hidden files

--- init.lua
+++ init.lua
@@ -267,7 +267,12 @@ require('lazy').setup({
     'stevearc/oil.nvim',
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
+    opts = {    
+      view_options = {
+      -- Show files and directories that start with "."
+      show_hidden = true,
+      },
+    },
     -- Optional dependencies
     -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
     dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
